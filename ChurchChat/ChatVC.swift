a7f142c984e901d124300c8519163ad9e7aa8c4a
Joel <pratt.joel@gmail.com>
2017-02-18 14:21:59 -0500

    update Podfile for Auth, Messages, and remote config

---
+++ ChurchChat/ChatVC.swift
@@ -0,0 +1,16 @@
+//
+//  ChatVC.swift
+//  ChurchChat
+//
+//  Created by Joel on 2/18/17.
+//  Copyright © 2017 Joel Pratt. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseGoogleAuthUI
+
+class ChatVC: UITableViewController {
+    
+}
--- Podfile
+++ Podfile
@@ -4,6 +4,12 @@ use_frameworks!
 platform :ios, '9.0'
 
 pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
+pod 'Firebase/Messaging'
+pod 'Firebase/RemoteConfig'
+pod 'Firebase/Storage'
+pod 'FirebaseUI'
 
 target 'ChurchChat' do
 
