ad1463d5623af592402415c6633c085ae73da9c3
Joel <pratt.joel@gmail.com>
2017-02-18 12:31:16 -0500

    Add Podfile

---
+++ Podfile
@@ -0,0 +1,10 @@
+# ChurchChat
+
+use_frameworks!
+platform :ios, '9.0'
+
+pod 'Firebase/Core'
+
+target 'ChurchChat' do
+
+end
---
+++ Pods/Target Support Files/Pods-ChurchChat/Pods-ChurchChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChurchChat : NSObject
+@end
+@implementation PodsDummy_Pods_ChurchChat
+@end
