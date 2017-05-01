499d790df5fe894aa796354318dbc6ba5177e3e6
Joel <pratt.joel@gmail.com>
2017-05-01 14:09:47 -0400

    Add initial contact form

--- ChurchChat/CommunicationFormVC.swift
+++ ChurchChat/CommunicationFormVC.swift
@@ -12,19 +12,38 @@ class CommunicationFormVC: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        form +++ Section("Section1")
+        setupForm()
+//        form +++ Section("Section1")
+//            <<< TextRow(){ row in
+//                row.title = "Name"
+//                row.placeholder = "Enter text here"
+//            }
+//            <<< PhoneRow(){
+//                $0.title = "Phone Number"
+//                $0.placeholder = "And numbers here"
+//            }
+//            +++ Section("Section2")
+//            <<< DateRow(){
+//                $0.title = "Date Row"
+//                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+//        }
+    }
+    
+    func setupForm() {
+        form +++ Section("Contact")
             <<< TextRow(){ row in
-                row.title = "Text Row"
-                row.placeholder = "Enter text here"
-            }
-            <<< PhoneRow(){
-                $0.title = "Phone Row"
-                $0.placeholder = "And numbers here"
-            }
-            +++ Section("Section2")
-            <<< DateRow(){
-                $0.title = "Date Row"
-                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+                row.title = "Name"
+                row.placeholder = "Enter name here"
+        }
+            <<< EmailRow(){
+                $0.title = "Email"
+                $0.placeholder = "Enter email here"
+        }
+        
+        +++ Section("Communication")
+            <<< TextAreaRow(){
+                $0.title = "Message"
+                $0.placeholder = "Enter your message here"
         }
     }
 }
