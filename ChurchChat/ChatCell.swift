cf36afd3f0faeaf05a0369714ae1e90c307e48ca
Joel <pratt.joel@gmail.com>
2017-09-20 14:20:06 -0400

    Update cell UI directly from cell

--- ChurchChat/ChatCell.swift
+++ ChurchChat/ChatCell.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class ChatCell: UITableViewCell {
     @IBOutlet weak var chatTitle: UILabel!
@@ -20,5 +21,42 @@ class ChatCell: UITableViewCell {
         self.chatImage.image = nil
         self.chatMessage.text = nil
         self.chatTitle.text = nil
+    }
+    
+    func updateUI(message: ChatMessage, indexPath: IndexPath, tableView: UITableView, room: String) {
+
+        let name = message.name ?? "username"
+        if let image = message.image {
+            chatImage.image = image
+            chatTitle.text = "From: \(name)"
+            
+        } else {
+            
+            if let photoUrl = message.url {
+                imageIndicator.isHidden = false
+                imageIndicator.startAnimating()
+                chatTitle.text = "From: \(name)"
+                FIRStorage.storage().reference(forURL: photoUrl).data(withMaxSize: INT64_MAX, completion: { (data, error) in
+                    guard error == nil else {
+                        print("error downloading image from photUrl: \(String(describing: error))")
+                        return
+                    }
+                    
+                    let messagePhoto = UIImage.init(data: data!, scale: 50)
+                    if self == tableView.cellForRow(at: indexPath) {
+                        DispatchQueue.main.async {
+                            self.imageIndicator.stopAnimating()
+                            self.chatImage.image = messagePhoto!
+                            ChatMessageStore.sharedInstance.updateMessageWithImage(name: room, index: indexPath.row, image: messagePhoto!)
+                            self.setNeedsLayout()
+                        }
+                    }
+                })
+            } else {
+                let text = message.text ?? "[message]"
+                chatTitle.text = name + ": "
+                chatMessage.text = text
+            }
+        }
     }
 }
--- ChurchChat/ChatTableDataSource.swift
+++ ChurchChat/ChatTableDataSource.swift
@@ -21,10 +21,10 @@ class ChatTableDataSource: NSObject, UITableViewDataSource {
     // MARK: - TableView DataSource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         
         if let messageCount = ChatMessageStore.sharedInstance.currentChatroom?.numberOfMessages {
-                return messageCount
+            return messageCount
         } else {
             return 0
         }
@@ -38,7 +38,7 @@ class ChatTableDataSource: NSObject, UITableViewDataSource {
         cell.imageIndicator.isHidden = true
         cell.imageIndicator.hidesWhenStopped = true
         return checkForImage(indexPath: indexPath, cell: cell, tableView: tableView)!
-
+        
     }
     
     // MARK: - TableView Helper Methods
@@ -50,41 +50,9 @@ class ChatTableDataSource: NSObject, UITableViewDataSource {
             print("no messages found in currentChatroom")
             return nil
         }
-       
+        
         let message = messages.chatRoom[indexPath.row]
-        let name = message.name ?? "username"
-        if let image = message.image {
-            cell.chatImage.image = image
-            cell.chatTitle.text = "From: \(name)"
-            
-        } else {
-            
-            if let photoUrl = message.url {
-                cell.imageIndicator.isHidden = false
-                cell.imageIndicator.startAnimating()
-                cell.chatTitle.text = "From: \(name)"
-                FIRStorage.storage().reference(forURL: photoUrl).data(withMaxSize: INT64_MAX, completion: { (data, error) in
-                    guard error == nil else {
-                        print("error downloading image from photUrl: \(String(describing: error))")
-                        return
-                    }
-                    
-                    let messagePhoto = UIImage.init(data: data!, scale: 50)
-                    if cell == tableView.cellForRow(at: indexPath) {
-                        DispatchQueue.main.async {
-                            cell.imageIndicator.stopAnimating()
-                            cell.chatImage.image = messagePhoto!
-                            ChatMessageStore.sharedInstance.updateMessageWithImage(name: self.chatRoom!, index: indexPath.row, image: messagePhoto!)
-                            cell.setNeedsLayout()
-                        }
-                    }
-                })
-            } else {
-                let text = message.text ?? "[message]"
-                cell.chatTitle.text = name + ": "
-                cell.chatMessage.text = text
-            }
-        }
+        cell.updateUI(message: message, indexPath: indexPath, tableView: tableView, room: chatRoom!)
         return cell
     }
 }
